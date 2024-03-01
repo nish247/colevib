83554142aad306d2e5e355d28405fe18de175378
nish247 <g2123046@fun.ac.jp>
2024-03-01 15:04:27 +0900

    deploy

--- src/count.jsx
+++ src/count.jsx
@@ -13,44 +13,33 @@ function Count() {
         setCountdown(prevCountdown => prevCountdown - 1);
       }, 1000));
     }
+    
+    // playSound(2000,100);
   };
 
-  const reset = () => {
-    setCountdown(5);
-    clearInterval(timer);
-    setTimer(null);
-  };
 
   useEffect(() => {
     // カウントダウンが0になったらタイマーをクリアする
     if (countdown === 0) {
+        playSound(2000, 100);
       clearInterval(timer);
       setTimer(null);
     }
   }, [countdown, timer]);
-  
-  // countdown が 0 になった後に実行するため、countdown の値を監視せず、timer だけを監視する
-  useEffect(() => {
-    // timer が null でなく、countdown が 0 の場合にのみサウンドを再生する
-    if (timer !== null && countdown === 0) {
-      for (let i = 0; i < 10; i++) {
-        playSound(2000, 100);
-      }
-    }
-  }, [timer]);
-  
 
   const playSound = (frequency, duration) => {
-    const oscillator = audioContext.createOscillator();
-    oscillator.type = 'sine'; // 波形を指定（ここではサイン波）
-    oscillator.frequency.setValueAtTime(frequency, audioContext.currentTime); // 周波数を設定
-    oscillator.connect(audioContext.destination); // 出力に接続
-    oscillator.start();        
-    // サウンド再生が終了した後にバイブレーションを再生
-    setTimeout(() => {
-      oscillator.stop(); // サウンド再生停止
-      vibrate(); // バイブレーション再生
-    }, duration);
+    for (let i = 0;i<10;i++){
+      const oscillator = audioContext.createOscillator();
+      oscillator.type = 'sine'; // 波形を指定（ここではサイン波）
+      oscillator.frequency.setValueAtTime(frequency, audioContext.currentTime); // 周波数を設定
+      oscillator.connect(audioContext.destination); // 出力に接続
+      oscillator.start();        
+      // サウンド再生が終了した後にバイブレーションを再生
+      setTimeout(() => {
+        oscillator.stop(); // サウンド再生停止
+        vibrate(); // バイブレーション再生
+      }, duration);
+  }
   };
 
   const vibrate = () => {
@@ -63,7 +52,6 @@ function Count() {
   return (
     <div>
       <button onClick={start}>スタート</button>
-      <button onClick={reset}>リセット</button>
       <div id="countdown">{countdown}</div>
     </div>
   );
